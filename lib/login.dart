78f325cf7d09809d0b25988e4558e69f194a51d9
이빵주 <yungju9982@naver.com>
2024-01-18 13:00:00 +0900

    feat: fix login ui

--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,8 @@ import 'package:peeka/Colors.dart';
 import 'package:peeka/Strings.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'NotRegisteredPage.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -11,38 +13,56 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.beige,
       body: SafeArea(
-        child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 60),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('assets/images/ic_logo.png'),
-              const SizedBox(height: 120), // 아이콘과 버튼 사이의 간격
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                child: InkWell(
-                  onTap: () {
-                    /** TODO 카카오 로그인 구현 */
-                  },
-                  child: Image.asset('assets/images/ic_kakao_login.png'),
+              Expanded(
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/ic_logo.png',
+                    height: 240,
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              const Text(Strings.agreementInfo,
-                  style: TextStyle(color: AppColors.g1)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    child: const Text(Strings.termsOfService,
-                        style: TextStyle(color: AppColors.g2)),
-                    onTap: () => launch(Strings.termsOfServiceUrl),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                    child: InkWell(
+                      onTap: () {
+                        /** TODO 카카오 로그인 구현 */
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NotRegisteredPage()),
+                        );
+                      },
+                      child: Image.asset(
+                        'assets/images/ic_kakao_login.png',
+                        height: 56,
+                      ),
+                    ),
                   ),
-                  const Text(Strings.and,
+                  const SizedBox(height: 20),
+                  const Text(Strings.agreementInfo,
                       style: TextStyle(color: AppColors.g1)),
-                  InkWell(
-                    child: const Text(Strings.privacyPolicy,
-                        style: TextStyle(color: AppColors.g2)),
-                    onTap: () => launch(Strings.privacyPolicyUrl),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        child: const Text(Strings.termsOfService,
+                            style: TextStyle(color: AppColors.g2)),
+                        onTap: () => launch(Strings.termsOfServiceUrl),
+                      ),
+                      const Text(Strings.and,
+                          style: TextStyle(color: AppColors.g1)),
+                      InkWell(
+                        child: const Text(Strings.privacyPolicy,
+                            style: TextStyle(color: AppColors.g2)),
+                        onTap: () => launch(Strings.privacyPolicyUrl),
+                      ),
+                    ],
                   ),
                 ],
               ),
